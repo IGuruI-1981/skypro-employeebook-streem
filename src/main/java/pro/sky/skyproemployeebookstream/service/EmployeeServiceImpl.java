88850229157d1ea0bdc37764cf68e7af7774e671
Viktor <viktor.ihtiyarov@gmail.com>
2022-11-06 23:27:50 +0700

    HomeWork2.10

--- src/main/java/pro/sky/skyproemployeebookstream/service/EmployeeServiceImpl.java
+++ src/main/java/pro/sky/skyproemployeebookstream/service/EmployeeServiceImpl.java
@@ -29,7 +29,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     public String hello() {
-        return "HelloSkyPRO";
+        return "HelloSkyPRO!";
     }
 
     public List<Employee> allEmployee() {
